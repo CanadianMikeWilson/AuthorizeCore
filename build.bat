524d1e81651260e562bb6c038652df824d8a562a
Mike Wilson <mwilson@brainfluid.ca>
2017-03-16 14:51:03 -0700

    Switch to msbuild runner

--- build.bat
+++ build.bat
@@ -21,7 +21,8 @@ REM - Option 1: Run dotnet build for every source folder in the project
 REM   e.g. call dotnet build <path> --configuration %config%
 REM - Option 2: Let msbuild handle things and build the solution
 REM "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MyLibrary.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-call dotnet build src\AuthorizeCore --configuration %config%
+REM call dotnet build src\AuthorizeCore --configuration %config%
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" AuthorizeCore.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
